8dba544b4fe6d8d6168bcdec24780b4354b682a2
Aries <Aries@iMac-3.local>
2024-02-26 17:15:06 -0600

    se añadio scrollbar

---
+++ src/components/ScrollBar.tsx
@@ -0,0 +1,19 @@
+import { motion, useScroll, useSpring } from "framer-motion";
+import "../styles/global.css";
+
+export default function App() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+
+  return (
+    <>
+      <motion.div className="progress-bar" style={{ scaleX }} />
+    </>
+  );
+}
+
+
